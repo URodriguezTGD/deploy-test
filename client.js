787943b1e67321abe0266e0f78528558410c1c92
gmonge <memomonge8@gmail.com>
2021-04-08 13:49:14 -0700

    Added .env file to gitignore, changed client configuration

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
-import sanityClient from '@sanity/client'
+import sanityClient from "@sanity/client";
 
 export default sanityClient({
-  projectId: 'l1738c4s', // you can find this in sanity.json
-  dataset: 'braintax-dataset', // or the name you chose in step 1
-  apiVersion: '2021-03-25',
-  useCdn: true // `false` if you want to ensure fresh data
-})
+    projectId: process.env.PROJECT_ID, 
+    dataset: process.env.DATASET,
+    apiVersion: process.env.API_VERSION,
+    token: process.env.SANITY_TOKEN,
+    useCdn: false,
+});
