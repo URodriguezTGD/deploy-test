0c746f1d8254e5d534c8ec969b44301d1ee4dc02
gmonge <memomonge8@gmail.com>
2021-04-09 10:24:57 -0700

    Added query to get article

--- pages/article/[id]/index.jsx
+++ pages/article/[id]/index.jsx
@@ -5,73 +5,54 @@ import client from "../../../client";
 import groq from "groq";
 
 const article = ({ article }) => {
-    // const router = useRouter();
-    // const {id} = router.query;
     return (
         <>
-            {/* <h1>{article.title}</h1>
-            <p>{article.body}</p> */}
-            <br />
+            {article.map(a => {
+                return(
+                    <div key={a.id}>
+                        <h1>{a.title}</h1>
+                        <div>
+                            {
+                                a.textContent !== null
+                                ?
+                                a.textContent.map(t => <p>{t}</p>)
+                                :
+                                'Este artículo aún no tiene contenido.'
+                            }
+                        </div>
+                    </div>
+                )
+            })}
             <Link href="/">Go back</Link>
         </>
     );
 };
 
-
-
-//   article.getInitialProps = async function (context) {
-//     // It's important to default the slug so that it doesn't return "undefined"
-
-//   }
-
-// article.getInitialProps = async (context) => {
-// console.log("🚀 ~ file: index.jsx ~ line 32 ~ getStaticProps ~ context", context)
-// console.log("🚀 ~ file: index.jsx ~ line 33 ~ getStaticProps ~ context", context.query)
-    
-//     // const { title = "" } = context.query;
-//     // return await client.fetch(query, { title });
-//     return {props:{}}
-
-//     // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${ context.params.id }`)
-//     // const article = await res.json();
-//     // return {
-//     //     props:{
-//     //         article
-//     //     }
-//     // }
-// };
-
 export const getStaticProps = async (context) => {
-    let article = {props:{}}
-    const query = groq`*[_type == "post"]`;
-    // const { title = "" } = query;
-    console.log("🚀 ~ file: index.jsx ~ line 52 ~ QUERY", query)
-    // console.log("🚀 ~ file: index.jsx ~ line 52 ~ TITLE", title)
-    
-    console.log("🚀 ~ file: index.jsx ~ line 32 ~ getStaticProps ~ context", context)
-    console.log("🚀 ~ file: index.jsx ~ line 33 ~ getStaticProps ~ context", context.query)
-    let response =  await client.fetch(query);
-    console.log("🚀 ~ file: index.jsx ~ line 59 ~ getStaticProps ~ RESPONSE:", response)
-    
-    //Add response to article object...
-    return article
-    
-        // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${ context.params.id }`)
-        // const article = await res.json();
-        // return {
-        //     props:{
-        //         article
-        //     }
-        // }
+    const query = groq `*[_type == "post" && slug.current == $slug]{
+        ...,
+        "id":_id,
+        title,
+        "textContent":body[_type match 'block' && children[0].text != ''].children[].text,
+        "slug":slug.current,
+    }`;
+    const params = {slug:context.params.id}
+    let article =  await client.fetch(query, params);
+    return {
+        props:{
+            article
+        }
+    }
     };
 
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const articles = await res.json();
-    const ids = articles.map((article) => article.id);
-    const paths = ids.map((id) => ({ params: { id: id.toString() } }));
-
+    const query = groq`*[_type == "post"][]{
+        "slug":slug.current
+    }`;
+    const articles = await client.fetch(query);
+    const slugs = articles.map((article) => article.slug);
+    const paths = slugs.map((slug) => ({ params: { id: slug } }));
     return {
         paths,
         fallback: false,
