e913083e81c02fd29d52fe0d4198aab97eda4b6a
gmonge <memomonge8@gmail.com>
2021-04-09 10:19:30 -0700

    Removed comments

--- components/ArticleGrid.jsx
+++ components/ArticleGrid.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import articleStyles from '../styles/Article.module.css';
 import { ArticleItem } from './ArticleItem';
 
+
+
 export const ArticleGrid = ({articles}) => {
     return (
         <div className={articleStyles.container}>
             <div className={articleStyles.grid}>
                 {
                     articles.map((article) => (<ArticleItem key={article.id} article={article} />)  )
+                    // console.log(`articles`, articles)
                 }
             </div>
         </div>
