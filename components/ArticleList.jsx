67258599de2ae2c2ffec2fc484189329eac92740
gmonge <memomonge8@gmail.com>
2021-04-13 15:26:25 -0700

    Changed the slider animation. Added left & right arrow to desktop version

--- components/ArticleList.jsx
+++ components/ArticleList.jsx
@@ -1,14 +1,49 @@
-import articleStyles from '../styles/Article.module.css';
-import { ArticleItem } from './ArticleItem';
-export const ArticleList = ({articles}) => {
+import { useEffect } from "react";
+import article from "../pages/article/[id]";
+import articleStyles from "../styles/Article.module.css";
+import { ArticleItem } from "./ArticleItem";
+export const ArticleList = ({ articles }) => {
+    useEffect(() => {
+        return () => {};
+    }, []);
+
     return (
         <div className={articleStyles.container}>
-            <h2 className='center'>Noticias</h2>
+            <h2 className="center">Noticias</h2>
             <div className={articleStyles.slider}>
-                {
-                    articles.map((article) => (<ArticleItem key={article.id} article={article} />)  )
-                }
+                <div
+                    id="articles-slider"
+                    style={{ display: "inline-flex", overflow: "scroll" }}
+                >
+                    {articles.map((article) => (
+                        <ArticleItem key={article.id} article={article} />
+                    ))}
+                </div>
+            </div>
+            <div className={articleStyles.sliderControls}>
+                <button className='btn-flat'
+                    onClick={(e) => {
+                        document.getElementById("articles-slider").scrollTo({
+                            top: 0,
+                            left:document.getElementById("articles-slider").scrollLeft - 1000,
+                            behavior: "smooth",
+                        });
+                    }}
+                >
+                    <span className='mdi mdi-arrow-left'></span>
+                </button>
+                <button className='btn-flat'
+                    onClick={(e) => {
+                        document.getElementById("articles-slider").scrollTo({
+                            top: 0,
+                            left:document.getElementById("articles-slider").scrollLeft + 1000,
+                            behavior: "smooth",
+                        });
+                    }}
+                >
+                    <span className='mdi mdi-arrow-right'></span>
+                </button>
             </div>
         </div>
-    )
-}
+    );
+};
