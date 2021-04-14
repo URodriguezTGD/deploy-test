c51ce91b9e48f2245f1ee5730d58ba0ab417e504
Jose Palomares <jpalomares@thegreendim.com>
2021-04-14 11:46:02 -0700

    Contact form now post data to a backend

--- components/FormContainer.jsx
+++ components/FormContainer.jsx
@@ -1,124 +1,159 @@
-import React,{ useEffect } from 'react';
-import FormContainerStyle from '../styles/FormContainer.module.css';
-import { useForm } from 'react-hook-form';
+import React, { useEffect } from "react";
+import FormContainerStyle from "../styles/FormContainer.module.css";
+import { useForm } from "react-hook-form";
 
 // imports i18n
-import { useRouter } from 'next/router';
-import es from '../locales/FormContainer/FormContainer_es';
-import en from '../locales/FormContainer/FormContainer_en';
+import { useRouter } from "next/router";
+import es from "../locales/FormContainer/FormContainer_es";
+import en from "../locales/FormContainer/FormContainer_en";
 
 export const FormContainer = () => {
-    // Selecting language
-    const router = useRouter();
-    const { locale } = router;
-    const t = locale === 'es' ? es : en;
+	// Selecting language
+	const router = useRouter();
+	const { locale } = router;
+	const t = locale === "es" ? es : en;
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
-    
-    const onSubmitForm = values => {
-        console.log(values);
-        reset();
-    };
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
 
-    return (
-        <div className={FormContainerStyle.formContainer}>
-            <form id="contactForm" className={FormContainerStyle.form} autoComplete="off" onSubmit={handleSubmit(onSubmitForm)}>
-                <div className={FormContainerStyle.row}>
-                    <input type="text" 
-                        id="fullName" 
-                        name="fullName" 
-                        placeholder={t.nombrePH} 
-                        {...register('fullName', 
-                        { 
-                            required: {
-                                value: true,
-                                message: `${t.nombreReq}`
-                            },
-                            maxLength: {
-                                value: 45,
-                                message: `${t.nombreMax}`
-                            },
-                            minLength: {
-                                value: 5,
-                                message: `${t.nombreMin}`
-                            },
-                            pattern: /[A-Za-z]+/
-                        })} />
-                    <label htmlFor="fullName">{t.nombre}</label>
-                    <span className={FormContainerStyle.error} >{errors?.fullName?.message}</span>
-                </div>
-                <div className={FormContainerStyle.row}>
-                    <input type="text" 
-                        id="email" 
-                        name="email" 
-                        placeholder={t.emailPH} 
-                        {...register('email', 
-                        {
-                            required: {
-                                value: true,
-                                message: `${t.emailReq}`
-                            },
-                            maxLength: 320,
-                            minLength: {
-                                value: 10,
-                                message: `${t.emailMin}`
-                            },
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: `${t.emailValid}`
-                              }
-                        })} />
-                    <label htmlFor="email">{t.email}</label>
-                    <span className={FormContainerStyle.error} >{errors?.email?.message}</span>
-                </div>
-                <div className={FormContainerStyle.row}>
-                    <input type="text" 
-                        id="asunto" 
-                        name="asunto" 
-                        placeholder={t.asuntoPH} 
-                        {...register('asunto', 
-                        {
-                            required: {
-                                value: true,
-                                message: `${t.asuntoReq}`
-                            },
-                            maxLength: {
-                                value: 100,
-                                message: `${t.asuntoMax}`
-                            },
-                            pattern: /[A-Za-z0-9.-\/]+/ 
-                        })} />
-                    <label htmlFor="asunto">{t.asunto}</label>
-                    <span className={FormContainerStyle.error} >{errors?.asunto?.message}</span>
-                </div>
-                <div className={FormContainerStyle.row}>
-                    <textarea id="message" 
-                        name="message" 
-                        placeholder={t.mensajePH} 
-                        {...register('message', 
-                        {
-                            required: {
-                                value: true,
-                                message: `${t.mensajeReq}`
-                            },
-                            maxLength: {
-                                value: 750,
-                                message: `${t.mensajeM}`
-                            },
-                            pattern: /[A-Za-z0-9.-\/]+/ 
-                        })} />
-                    <label htmlFor="message">{t.mensaje}</label>
-                    <span className={FormContainerStyle.error}>{errors?.message?.message}</span>
-                </div>
-                <div className={FormContainerStyle.row}>
-                    <input type="checkbox" name="terms" id="terms" required/>
-                    <label htmlFor="terms"></label>
-                    <label className={FormContainerStyle.terms}>{t.acepto} <a href={t.privacidadLink}>{t.privacidad}</a></label>
-                </div>
-                <div className={FormContainerStyle.row}>
-                    <button type="submit">{t.boton}</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+	const onSubmitForm = async (values) => {
+		let result = await fetch(`/api/addContact`, {
+			method: "post",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(values),
+		});
+		console.log(`result`, result);
+		if (!result.ok) {
+			console.log(`error ${result.status}: ${result.statusText}`);
+		} else {
+			reset();
+		}
+	};
+
+	return (
+		<div className={FormContainerStyle.formContainer}>
+			<form
+				id="contactForm"
+				className={FormContainerStyle.form}
+				autoComplete="off"
+				onSubmit={handleSubmit(onSubmitForm)}
+			>
+				<div className={FormContainerStyle.row}>
+					<input
+						type="text"
+						id="fullName"
+						name="fullName"
+						placeholder={t.nombrePH}
+						{...register("fullName", {
+							required: {
+								value: true,
+								message: `${t.nombreReq}`,
+							},
+							maxLength: {
+								value: 45,
+								message: `${t.nombreMax}`,
+							},
+							minLength: {
+								value: 5,
+								message: `${t.nombreMin}`,
+							},
+							pattern: /[A-Za-z]+/,
+						})}
+					/>
+					<label htmlFor="fullName">{t.nombre}</label>
+					<span className={FormContainerStyle.error}>
+						{errors?.fullName?.message}
+					</span>
+				</div>
+				<div className={FormContainerStyle.row}>
+					<input
+						type="text"
+						id="email"
+						name="email"
+						placeholder={t.emailPH}
+						{...register("email", {
+							required: {
+								value: true,
+								message: `${t.emailReq}`,
+							},
+							maxLength: 320,
+							minLength: {
+								value: 10,
+								message: `${t.emailMin}`,
+							},
+							pattern: {
+								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+								message: `${t.emailValid}`,
+							},
+						})}
+					/>
+					<label htmlFor="email">{t.email}</label>
+					<span className={FormContainerStyle.error}>
+						{errors?.email?.message}
+					</span>
+				</div>
+				<div className={FormContainerStyle.row}>
+					<input
+						type="text"
+						id="asunto"
+						name="asunto"
+						placeholder={t.asuntoPH}
+						{...register("asunto", {
+							required: {
+								value: true,
+								message: `${t.asuntoReq}`,
+							},
+							maxLength: {
+								value: 100,
+								message: `${t.asuntoMax}`,
+							},
+							pattern: /[A-Za-z0-9.-\/]+/,
+						})}
+					/>
+					<label htmlFor="asunto">{t.asunto}</label>
+					<span className={FormContainerStyle.error}>
+						{errors?.asunto?.message}
+					</span>
+				</div>
+				<div className={FormContainerStyle.row}>
+					<textarea
+						id="message"
+						name="message"
+						placeholder={t.mensajePH}
+						{...register("message", {
+							required: {
+								value: true,
+								message: `${t.mensajeReq}`,
+							},
+							maxLength: {
+								value: 750,
+								message: `${t.mensajeM}`,
+							},
+							pattern: /[A-Za-z0-9.-\/]+/,
+						})}
+					/>
+					<label htmlFor="message">{t.mensaje}</label>
+					<span className={FormContainerStyle.error}>
+						{errors?.message?.message}
+					</span>
+				</div>
+				<div className={FormContainerStyle.row}>
+					<input type="checkbox" name="terms" id="terms" required />
+					<label htmlFor="terms"></label>
+					<label className={FormContainerStyle.terms}>
+						{t.acepto} <a href={t.privacidadLink}>{t.privacidad}</a>
+					</label>
+				</div>
+				<div className={FormContainerStyle.row}>
+					<button type="submit">{t.boton}</button>
+				</div>
+			</form>
+		</div>
+	);
+};
--- pages/api/addContact.js
+++ pages/api/addContact.js
@@ -1,25 +1,36 @@
 export default async (req, res) => {
-	const mutations = [
-		{
-			create: {
-				
-			},
-		},
-	];
-    
-	let result = await fetch(
-		`https://${process.env.PROJECT_ID}.api.sanity.io/v${process.env.API_VERSION}/data/mutate/${process.env.DATASET}`,
-		{
-			method: "post",
-			headers: {
-				"Content-type": "application/json",
-				Authorization: `Bearer ${process.env.SANITY_TOKEN}`,
-			},
-			body: JSON.stringify({ mutations }),
-		},
-	);
-	console.log(result);
-	res.status(200).json(result);
+	switch (req.method) {
+		case "POST":
+			const mutations = [
+				{
+					create: {
+						_type: "contact",
+						name: req.body.fullName,
+						email: req.body.email,
+						subject: req.body.asunto,
+						message: req.body.message,
+					},
+				},
+			];
+
+			let result = await fetch(
+				`https://${process.env.PROJECT_ID}.api.sanity.io/v${process.env.API_VERSION}/data/mutate/${process.env.DATASET}`,
+				{
+					method: "post",
+					headers: {
+						"Content-type": "application/json",
+						Authorization: `Bearer ${process.env.SANITY_TOKEN}`,
+					},
+					body: JSON.stringify({ mutations }),
+				},
+			);
+			// console.log(result);
+			res.status(200).json(result);
+			break;
+		default:
+			res.status(405).end(); //Method Not Allowed
+			break;
+	}
 	// .then((response) => response.json())
 	// .then((result) => res.status(200).json(result))
 	// .catch((error) => res.status(500).json(error));
