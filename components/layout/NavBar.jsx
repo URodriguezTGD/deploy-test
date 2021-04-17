0641ca9852f64ad8e84e2a11fbd540cdd86e351e
Ulises Rodriguez <ulises.rodriguez.bjrqz@gmail.com>
2021-04-16 17:58:02 -0700

    updated navbar - es/en oculted in articles

--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Logo1 from "../brand/Logo1";
 import Logo2 from "../brand/Logo2";
+import Link from 'next/link';
 
 // imports i18n
 import { useRouter } from 'next/router';
@@ -25,7 +26,7 @@ export default function NavBar() {
             <nav>
                 <div className={ menuClicked ? 'nav-wrapper': "nav-wrapper container" }>
                     <a
-                        href={ locale === 'es' ? '/' : '/en' }
+                        href={ locale === 'es' ? '/es' : '/en' }
                         className="brand-logo center blue-text hide-on-small-only"
                         style={{ paddingTop: ".2em" }}
                     >
@@ -41,39 +42,39 @@ export default function NavBar() {
                     </a>
                     <ul className="left hide-on-med-and-down">
                         <li>
-                            <a href={ locale === 'es' ? '/' : '/en' }>{t.inicio}</a>
+                            <a href={ locale === 'es' ? '/es' : '/en' }>{t.inicio}</a>
                         </li>
                         <li>
-                            <a href={ locale === 'es' ? '/services' : '/en/services' }>{t.servicios}</a>
+                            <a href={ locale === 'es' ? '/es/services' : '/en/services' }>{t.servicios}</a>
                         </li>
                     </ul>
                     <ul className="right hide-on-med-and-down">
                         <li>
-                            <a href={ locale === 'es' ? '/blog' : '/en/blog' }>{t.blog}</a>
+                            <a href={ locale === 'es' ? '/es/blog' : '/en/blog' }>{t.blog}</a>
                         </li>
                         <li>
-                            <a href={ locale === 'es' ? '/contact' : '/en/contact' }>{t.contacto}</a>
+                            <a href={ locale === 'es' ? '/es/contact' : '/en/contact' }>{t.contacto}</a>
                         </li>
                         <li>
-                            <a className="selector" onClick={changeLanguage}>{locale}</a>
+                            {router.pathname === "/article/[id]" ? null : <a className="selector" onClick={changeLanguage}>{locale}</a>}
                         </li>
                     </ul>
                     
                     <ul className={ menuClicked ? 'nav-menu-mobile active' : "nav-menu-mobile" }>
                         <li>
-                            <a href={ locale === 'es' ? '/' : '/en' }><span className='mdi mdi-home ham'></span>{t.inicio}</a>
+                            <a href={ locale === 'es' ? '/es' : '/en' }><span className='mdi mdi-home ham'></span>{t.inicio}</a>
                         </li>
                         <li>
-                            <a href={ locale === 'es' ? '/services' : '/en/services' }><span className='mdi mdi-scale-balance ham'></span>{t.servicios}</a>
+                            <a href={ locale === 'es' ? '/es/services' : '/en/services' }><span className='mdi mdi-scale-balance ham'></span>{t.servicios}</a>
                         </li>
                         <li className="only-mobile">
-                            <a href={ locale === 'es' ? '/blog' : '/en/blog' }><span className='mdi mdi-post ham'></span>{t.blog}</a>
+                            <a href={ locale === 'es' ? '/es/blog' : '/en/blog' }><span className='mdi mdi-post ham'></span>{t.blog}</a>
                         </li>
                         <li className="only-mobile">
-                            <a href={ locale === 'es' ? '/contact' : '/en/contact' }><Logo2 className="logoMenu" width={43} height={36}/>{t.contacto}</a>
+                            <a href={ locale === 'es' ? '/es/contact' : '/en/contact' }><Logo2 className="logoMenu" width={43} height={36}/>{t.contacto}</a>
                         </li>
                         <li>
-                            <a className="selector" onClick={changeLanguage}>{locale}</a>
+                            {router.pathname === "/article/[id]" ? null : <a className="selector" onClick={changeLanguage}>{locale}</a>}
                         </li>
                     </ul>
                 </div>
