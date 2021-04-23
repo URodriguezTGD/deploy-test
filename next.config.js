55c1473b368ac61f9775619b656b637bd820be9a
Jose Palomares <jpalomares@thegreendim.com>
2021-04-23 14:16:22 -0700

    Modified amplify config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-    i18n: {
-      locales: ['es', 'en'],
-      defaultLocale: 'es',
-    },
-  };
+	i18n: {
+		locales: ["es", "en"],
+		defaultLocale: "es",
+	},
+	serverRuntimeConfig: {
+		projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+		dataset: process.env.NEXT_PUBLIC_DATASET,
+		apiVersion: process.env.API_VERSION,
+		token: process.env.SANITY_TOKEN,
+	},
+};
